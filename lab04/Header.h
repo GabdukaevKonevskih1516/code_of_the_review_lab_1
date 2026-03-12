15596484bea4f99a9a96b8b84b9ba37bdf2d4799
GabdukaevKonevskih1516 <konevskihruslan@gmail.com>
2026-03-12 15:33:29 +0500

    Header.h

--- lab04/Header.h
+++ lab04/Header.h
@@ -1,1 +1,13 @@
+#pragma once
 
+#include <iostream>
+#include <string>
+
+// Проверяет строку на наличие недопустимых символов и баланс скобок.
+bool IsInputValid(const std::string & InS);
+
+// Основная рекурсивная функция вычисления выражения.
+int CalculateExpression(std::string InExpression);
+
+// Вспомогательная рекурсивная функция для поиска оператора во время разбора.
+int FindOperatorRecursive(const std::string& InS, int InIndex, int InLevel, const std::string& InOps);
