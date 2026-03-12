cb7839e4b0bb97586d3f3851358506c186066d5d
GabdukaevKonevskih1516 <konevskihruslan@gmail.com>
2026-03-12 15:29:26 +0500

    main.cpp

--- lab03/main.cpp
+++ lab03/main.cpp
@@ -1,1 +1,12 @@
+#include "Header.h"
 
+int main()
+{
+    // FIX_ME: Отсутствие локали и вызов логики прямо в main
+    // setlocale полезен для вывода
+    std::setlocale(LC_ALL, "ru");
+
+    SolveKNumericalTask();
+
+    return 0;
+}
