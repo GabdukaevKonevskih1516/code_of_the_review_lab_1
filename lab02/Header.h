62a5feaa504bc6846085e86ca79bd7e6777d471d
GabdukaevKonevskih1516 <konevskihruslan@gmail.com>
2026-03-12 15:27:49 +0500

    Header.h

--- lab02/Header.h
+++ lab02/Header.h
@@ -1,1 +1,13 @@
+#pragma once
 
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+// Проверка размера доски на соответствие условию(1 < N < 80) 
+bool IsBoardSizeValid(int InN);
+
+// Основная функция решения задачи о Хромом короле 
+void SolveLameKingTask();
