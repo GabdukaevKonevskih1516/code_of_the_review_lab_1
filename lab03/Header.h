0e3ae8b4ae5533ef7b7ff80172d06a3dbfa16e24
GabdukaevKonevskih1516 <konevskihruslan@gmail.com>
2026-03-12 15:30:34 +0500

    Header.h

--- lab03/Header.h
+++ lab03/Header.h
@@ -1,1 +1,10 @@
+#pragma once
 
+#include <iostream>
+#include <iomanip>
+
+// Проверка входных параметров согласно условию задачи 
+bool IsInputValid(int InK, int InN);
+
+// Основная функция решения задачи о K - ичных числах 
+void SolveKNumericalTask();
