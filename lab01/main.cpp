a52907f618580c4daf3469b0fd315f4eb9ac3062
GabdukaevKonevskih1516 <konevskihruslan@gmail.com>
2026-03-12 15:21:31 +0500

    main.cpp

--- lab01/main.cpp
+++ lab01/main.cpp
@@ -1,1 +1,15 @@
+#include "Header.h"
 
+int main()
+{
+    // Настройка локали для корректного отображения кириллицы
+    setlocale(LC_ALL, "ru");
+
+    // FIX_ME: Отсутствие std::, несколько операторов на одной строке
+    // ind_i = i; ind_j = j; weight = main_vector[i][j]; volume = sum_volume_vector[i][j];
+
+    SolveArchaeologistTask();
+
+    std::cout << std::endl;
+    return 0;
+}
