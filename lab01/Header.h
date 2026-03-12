94887785e6986f44000e05a173b75865e9248b5d
GabdukaevKonevskih1516 <konevskihruslan@gmail.com>
2026-03-12 15:25:05 +0500

    Header.h

--- lab01/Header.h
+++ lab01/Header.h
@@ -1,1 +1,16 @@
+#pragma once
 
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <climits>
+
+// Проверка валидности количества предметов 
+bool IsCountValid(int InCount);
+
+// Проверка валидности веса 
+bool IsWeightValid(int InWeight);
+
+// Основная функция решения задачи археолога 
+void SolveArchaeologistTask();
